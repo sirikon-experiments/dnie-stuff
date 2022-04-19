b02ba83bc2242e17baf2a9340afb979c9ef89b71
Carlos Fdez. Llamas <hello@sirikon.me>
2022-04-19 18:07:00 +0200

    cosas

--- login-server/install.sh
+++ login-server/install.sh
@@ -14,8 +14,16 @@ echo "import $(pwd)/Caddyfile" > /etc/caddy/Caddyfile
 curl -Lo "fnmt.cer" "https://www.sede.fnmt.gob.es/documents/10445900/10526749/AC_Raiz_FNMT-RCM_SHA256.cer"
 openssl x509 -inform der -in fnmt.cer -out fnmt.pem
 
-curl -Lo "dnie.zip" "https://www.dnielectronico.es/ZIP/ACRAIZ-DNIE2.zip"
-unzip "dnie.zip"
-mv *.crt dnie.pem
+curl -Lo "dnie-04.zip" "https://www.dnielectronico.es/ZIP/ACDNIE004.crt.zip"
+unzip "dnie-04.zip"
+mv *.crt dnie-004.pem
+
+curl -Lo "dnie-05.zip" "https://www.dnielectronico.es/ZIP/ACDNIE005.crt.zip"
+unzip "dnie-05.zip"
+mv *.crt dnie-005.pem
+
+curl -Lo "dnie-05.zip" "https://www.dnielectronico.es/ZIP/ACDNIE006.crt.zip"
+unzip "dnie-06.zip"
+mv *.crt dnie-006.pem
 
 systemctl restart caddy
