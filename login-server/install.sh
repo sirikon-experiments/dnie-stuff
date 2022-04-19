2073550e4e6d0ad71f13b5e27203c97feca4395d
Carlos Fdez. Llamas <hello@sirikon.me>
2022-04-19 17:45:45 +0200

    lala

--- login-server/install.sh
+++ login-server/install.sh
@@ -2,12 +2,16 @@
 set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+apt update
+apt upgrade -y
+apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
-sudo apt install -y caddy
+apt update
+apt install -y caddy
 
 echo "import $(pwd)/Caddyfile" > /etc/caddy/Caddyfile
+curl -Lo "fnmt.cer" "https://www.sede.fnmt.gob.es/documents/10445900/10526749/AC_Raiz_FNMT-RCM_SHA256.cer"
+
+
+systemctl restart caddy
