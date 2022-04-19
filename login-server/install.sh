863300d7b11e9ac2d61a11e51fdd35ad1a3ced19
Carlos Fdez. Llamas <hello@sirikon.me>
2022-04-19 18:10:44 +0200

    lala

--- login-server/install.sh
+++ login-server/install.sh
@@ -2,16 +2,17 @@
 set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-apt update
-apt upgrade -y
-apt install -y debian-keyring debian-archive-keyring apt-transport-https unzip
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-apt update
-apt install -y caddy
+# apt update
+# apt upgrade -y
+# apt install -y debian-keyring debian-archive-keyring apt-transport-https unzip
+# curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
+# curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+# apt update
+# apt install -y caddy
 
 rm *.zip
 rm *.pem
+rm *.crt
 
 echo "import $(pwd)/Caddyfile" > /etc/caddy/Caddyfile
 curl -Lo "fnmt.cer" "https://www.sede.fnmt.gob.es/documents/10445900/10526749/AC_Raiz_FNMT-RCM_SHA256.cer"
