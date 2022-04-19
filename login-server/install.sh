4ca308c4b72a1d2c6e5df4307c93a35616ff08eb
Carlos Fdez. Llamas <hello@sirikon.me>
2022-04-19 17:33:03 +0200

    lala

--- login-server/install.sh
+++ login-server/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 sudo apt update
 sudo apt upgrade -y
@@ -8,3 +9,5 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /et
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update
 sudo apt install -y caddy
+
+echo "import $(pwd)/Caddyfile" > /etc/caddy/Caddyfile
